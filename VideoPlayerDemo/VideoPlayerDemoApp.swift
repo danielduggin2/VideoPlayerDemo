3dc20bc147bb828041a0d3c50da082a055fd0f36
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2023-02-02 19:08:31 -0500

    Initial Commit

---
+++ VideoPlayerDemo/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by Daniel Wade Duggin on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
